a60c1fdc2c038cbd418698920f400247b25cf530
jeffyxia <xiajiefeiemail@163.com>
2017-07-09 00:12:23 +0800

    PhantomJS

--- book/spiders/JD.py
+++ book/spiders/JD.py
@@ -3,12 +3,13 @@ import scrapy
 from selenium import webdriver
 from book.items import BookItem
 import re
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class JdSpider(scrapy.Spider):
     name = "JD"
     allowed_domains = ["jd.com"]
-    start_urls = ['http://book.jd.com/']
+    start_urls = []
     rankTypeSuffixes = ['0-0-0','0-1-0'] # 0-0-0 means bestsellers, 0-1-0 means new newhotsales
     rankFreqSuffixes = ['10001','10003'] # 10001 means ranking list of 24 hours, 10003 means ranking list of 30 days
 
@@ -16,44 +17,61 @@ class JdSpider(scrapy.Spider):
         for rankFreqSuffix in rankFreqSuffixes:
         	start_urls.append('http://book.jd.com/booktop/' + rankTypeSuffix + '.html?category=1713-' + rankTypeSuffix + '-' + rankFreqSuffix + '-1#comfort')
 
+    def __init__(self):
+        dcap = dict(DesiredCapabilities.PHANTOMJS)
+        dcap["phantomjs.page.settings.userAgent"] = ("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0")
+        dcap["phantomjs.page.settings.loadImages"] = False
+        self.driver = webdriver.PhantomJS(desired_capabilities=dcap)
+
+
     def parse(self, response):
-        books = response.xpath('//div[@class="m m-list"]')
+        self.driver.set_page_load_timeout(20)
+        self.driver.get(response.url)
+        books = self.driver.find_elements_by_xpath('//div[@class="m m-list"]//li/div[@class="p-img"]/a')
+
+        for book in books:
+            yield scrapy.Request(url = book.get_attribute("href"), callback = self.parse_item)
+        self.dirver.quit()
+
+    # def parse(self, response):
+    #     books = response.xpath('//div[@class="m m-list"]')
+    #     items = []
+    #     for i in range(0,19):
+    #         item = BookItem()
+    #         url = 'http:' + books.xpath('.//li/div[@class="p-img"]/a/@href').extract()[i]
+    #
+    #         yield scrapy.Request(url = 'http:'+ url, callback = self.parse_item)
+    #
+    def parse_item(self, response):
+
+        self.sub_driver = webdriver.PhantomJS()
+        self.sub_driver.get(response.url)
+
         items = []
-        for i in range(0,19):
-            item = BookItem()
-            url = 'http:' + books.xpath('.//li/div[@class="p-img"]/a/@href').extract()[i]
-            print(url)
-            # yield scrapy.Request(url = 'http:'+ url, callback = self.parse_item)
-            item = PhantomJSCrawl(url)
-            items.append(item)
+        item = BookItem()
+        item['title'] = self.sub_driver.find_element_by_xpath('//div[@id="name"]/h1').text
+        item['author'] = self.sub_driver.find_element_by_xpath('//div[@id="name"]/div[@class="p-author"]/a').text
+        item['image'] = self.sub_driver.find_element_by_xpath('//div[@id="preview"]//img').get_attribute('src')
+
+        price = self.sub_driver.find_element_by_id("jd-price").text
+        item['price'] = re.findall(re.compile(r"(\d+(\.\d+)?)"), price.encode("utf-8"))[0][0]
+
+        parameter2 = self.sub_driver.find_element_by_id('parameter2')
+        item['publisher'] = parameter2.find_elements_by_xpath('.//li')[0].get_attribute('title')
+        item['ISBN'] = parameter2.find_elements_by_xpath('.//li')[1].get_attribute('title')
+
+        breadcrumb = self.sub_driver.find_element_by_xpath('//div[@class="breadcrumb"]')
+        item['tag0'] = breadcrumb.find_elements_by_xpath('.//a')[0].text
+        item['tag1'] = breadcrumb.find_elements_by_xpath('.//a')[1].text
+        item['tag2'] = breadcrumb.find_elements_by_xpath('.//a')[2].text
+
+        item['link'] = response.url
+        item['web'] = 'JD'
+
+        self.sub_driver.quit()
+
+        items.append(item)
         return items
-
-def PhantomJSCrawl(url):
-    driver = webdriver.Chrome()
-    driver.get(url)
-
-    item = BookItem()
-    item['title'] = driver.find_element_by_xpath('//div[@id="name"]/h1').text
-    item['author'] = driver.find_element_by_xpath('//div[@id="name"]/div[@class="p-author"]/a').text
-    item['image'] = driver.find_element_by_xpath('//div[@id="preview"]//img').get_attribute('src')
-
-    price = driver.find_element_by_id("jd-price").text
-    item['price'] = re.findall(re.compile(r"(\d+(\.\d+)?)"), price.encode("utf-8"))[0][0]
-
-    parameter2 = driver.find_element_by_id('parameter2')
-    item['publisher'] = parameter2.find_elements_by_xpath('.//li')[0].get_attribute('title')
-    item['ISBN'] = parameter2.find_elements_by_xpath('.//li')[1].get_attribute('title')
-
-    breadcrumb = driver.find_element_by_xpath('//div[@class="breadcrumb"]')
-    item['tag0'] = breadcrumb.find_elements_by_xpath('.//a')[0].text
-    item['tag1'] = breadcrumb.find_elements_by_xpath('.//a')[1].text
-    item['tag2'] = breadcrumb.find_elements_by_xpath('.//a')[2].text
-
-    item['link'] = url
-    item['web'] = 'JD'
-
-    driver.quit()
-    return item
 
     # def parse_item(self,response):
     #
--- book/spiders/SearchJD.py
+++ book/spiders/SearchJD.py
@@ -4,51 +4,57 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from book.items import BookItem
 import re
+import time
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
 
 class SearchjdSpider(scrapy.Spider):
     name = "SearchJD"
     allowed_domains = ["jd.com"]
     start_urls = ['https://book.jd.com']
 
-    def search(self, response):
-        driver = webdriver.Chrome().get(response.url)
+    def __init__(self):
+        dcap = dict(DesiredCapabilities.PHANTOMJS)
+        dcap["phantomjs.page.settings.userAgent"] = ("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0")
+        dcap["phantomjs.page.settings.loadImages"] = False
+        self.driver = webdriver.PhantomJS(desired_capabilities=dcap)
 
-        element = driver.find_element_by_id("key")
+        
+    def parse(self, response, ISBN):
+        self.driver.set_page_load_timeout(20)
+        self.driver.get(response.url)
+
+        element = self.driver.find_element_by_id("key")
         element.clear()
-        element.send_keys("9787555107408")
+        element.send_keys(ISBN)
         element.send_keys(Keys.ENTER)
 
-        url = driver.find_element_by_xpath('//ul[@class="gl-warp clearfix"]//div[@class="p-img"]/a').get_attribute("href")
-        driver.quit()
+        time.sleep(1)
 
-        item = PhantomJSCrawl(url)
+        url = self.driver.find_element_by_xpath('//ul[@class="gl-warp clearfix"]//div[@class="p-img"]/a').get_attribute("href")
+        # self.driver.switch_to_window(self.driver.window_handles[1])
 
+        self.driver.get(url)
+        item = BookItem()
+        item['title'] = self.driver.find_element_by_xpath('//div[@id="name"]/h1').text
+        item['author'] = self.driver.find_element_by_xpath('//div[@id="name"]/div[@class="p-author"]/a').text
+        item['image'] = self.driver.find_element_by_xpath('//div[@id="preview"]//img').get_attribute('src')
+
+        price = self.driver.find_element_by_id("jd-price").text
+        item['price'] = re.findall(re.compile(r"(\d+(\.\d+)?)"), price.encode("utf-8"))[0][0]
+
+        parameter2 = self.driver.find_element_by_id('parameter2')
+        item['publisher'] = parameter2.find_elements_by_xpath('.//li')[0].get_attribute('title')
+        ISBN = parameter2.find_elements_by_xpath('.//li')[1].get_attribute('title')
+        item['ISBN'] = filter(str.isdigit, ISBN.encode("utf-8")).decode("utf-8")
+
+        breadcrumb = self.driver.find_element_by_xpath('//div[@class="breadcrumb"]')
+        item['tag0'] = breadcrumb.find_elements_by_xpath('.//a')[0].text
+        item['tag1'] = breadcrumb.find_elements_by_xpath('.//a')[1].text
+        item['tag2'] = breadcrumb.find_elements_by_xpath('.//a')[2].text
+
+        item['link'] = self.driver.current_url
+        item['web'] = 'JD'
+
+        self.driver.quit()
         return item
-
-
-def PhantomJSCrawl(url):
-    driver = webdriver.Chrome().get(url)
-
-    item = BookItem()
-    item['title'] = driver.find_element_by_xpath('//div[@id="name"]/h1').text
-    item['author'] = driver.find_element_by_xpath('//div[@id="name"]/div[@class="p-author"]/a').text
-    item['image'] = driver.find_element_by_xpath('//div[@id="preview"]//img').get_attribute('src')
-
-    price = driver.find_element_by_id("jd-price").text
-    item['price'] = re.findall(re.compile(r"(\d+(\.\d+)?)"), price.encode("utf-8"))[0][0]
-
-    parameter2 = driver.find_element_by_id('parameter2')
-    item['publisher'] = parameter2.find_elements_by_xpath('.//li')[0].get_attribute('title')
-    ISBN = parameter2.find_elements_by_xpath('.//li')[1].get_attribute('title')
-    item['ISBN'] = filter(str.isdigit, ISBN)
-
-    breadcrumb = driver.find_element_by_xpath('//div[@class="breadcrumb"]')
-    item['tag0'] = breadcrumb.find_elements_by_xpath('.//a')[0].text
-    item['tag1'] = breadcrumb.find_elements_by_xpath('.//a')[1].text
-    item['tag2'] = breadcrumb.find_elements_by_xpath('.//a')[2].text
-
-    item['link'] = url
-    item['web'] = 'JD'
-
-    driver.quit()
-    return item
